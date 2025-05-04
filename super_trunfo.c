40b5cc7e15e8cf910c7758568c099afe3f3bdcc1
Caio Rodrigues <124935336+scaiorodrigues@users.noreply.github.com>
2025-05-04 15:33:47 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,21 +1,25 @@
 int main() {
     // Carta 1
     char estado1 = "A";
-    char codigo1[] = "A01";
-    char cidade1[] = "São Paulo";
+    char codigo1[5] = "A01";
+    char cidade1[30] = "São Paulo";
     int populacao1 = 12325000;
     float area1 = 1521.11;
     float pib1 = 699.28;
     int ptsTuristicos1 = 50;
+    float densidade1 = populacao1 / area1;
+    float pib_per_capital1 = (pib1 * 1000000000) / populacao1;
 
     // Carta 2
     char estado2 = "B";
-    char codigo2[] = "A02";
-    char cidade2[] = "Rio de Janeiro";
+    char codigo2[5] = "B02";
+    char cidade2[30] = "Rio de Janeiro";
     int populacao2 = 678000;
     float area2 = 1200.25;
     float pib2 = 300.50;
     int ptsTuristicos2 = 30;
+    float densidade2 = populacao2 / area2;
+    float pib_per_capita2 = (pib2 * 1000000000) / populacao2;
 
     // Exibição de dados da Carta 1
     printf("Carta 1:\n");
@@ -26,6 +30,8 @@ int main() {
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", ptsTuristicos1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
+    printf("PIB per Capita: %.2f reais\n\n", pib_per_capita1);
 
     printf("\n");
 
@@ -36,8 +42,10 @@ int main() {
     printf("Nome da Cidade: %s\n", cidade2);
     printf("População: %d\n", populacao2);
     printf("Área: %.2f km²\n", area2);
-    printf("PIB: %.2f bilhôes de reais\n", pib2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", ptsTuristicos2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
+    printf("PIB per Capita: %.2f reais\n", pib_per_capita2);
 
     return 0;
 }
